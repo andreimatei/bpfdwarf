cb2919a724f23c64ba858fc0351dee929fb45c4a
Andrei Matei <andreimatei1@gmail.com>
2020-12-13 12:18:01 -0500

    improve example app

--- example/example.c
+++ example/example.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int my_func(int i) {
-    fprintf(stderr, ".");
+int __attribute__ ((noinline)) my_func(int i) {
+	fprintf(stderr, ".");
 }
 
 int main(int argc, char **argv) {
     for (int i = 0;;++i) {
-        my_func(i);
-        sleep(1);
-    }
+			fprintf(stdout, "calling my_func(%d)\n", i);
+			my_func(i);
+			sleep(1);
+		}
 }
 
